b13b7f2dce51968d0b7c37050c1ab90fd2db43e5
Juan Alvarez Arquillos <arquillos@gmail.com>
2025-09-04 21:48:33 +0200

    Fixed buildUrl

--- tests/utils/urlBuilder.ts
+++ tests/utils/urlBuilder.ts
@@ -2,11 +2,15 @@ import pageUrls from '../utils/pagesUrls';
 
 // Create the URL dinamically
 export function buildUrl(page: string, params?: Record<any, any>) {
-    const uiPath = pageUrls[page];
+    console.log('Building URL. page: ' + page)
+
+    const uiPath = pageUrls[page.replace('/', '')];
+    console.log('uiPath: ' + uiPath)
 
     const qParams = new URLSearchParams(params);
 
     const url = params ? `${uiPath.concat('?')}${qParams.toString()}` : uiPath;
+    console.log('URL: ' + url)
 
     /**
         * page  bookStore
